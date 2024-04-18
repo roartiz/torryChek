b35b510efaab09a859fdececa5abbb92c839bfb8
roartiz <63872939+roartiz@users.noreply.github.com>
2024-04-17 21:06:01 -0400

    Add files via upload

---
+++ toriychek.py
@@ -0,0 +1,14 @@
+# toriyChek v1.0
+# skreamapillar
+# 04.13.24
+
+# Define variables
+brave = 'C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe'
+toriyList = ["torrentleech.org", "iptorrents.com", "filelist.io", "cinemaZ.to", "avistaZ.to", "exoticaZ.to", "animetorrents.me", "myanonamouse.net", "audionews.org"]
+
+
+print(brave)
+print(toriyList)
+f = open("brave.exe")
+
+# Windows
