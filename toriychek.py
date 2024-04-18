1e1a6f92cb27888ff571967cff6d98d2b1c1da1c
roartiz <63872939+roartiz@users.noreply.github.com>
2024-04-17 22:01:25 -0400

    Add files via upload

--- toriychek.py
+++ toriychek.py
@@ -1,14 +1,31 @@
-# toriyChek v1.0
-# skreamapillar
-# 04.13.24
-
-# Define variables
-brave = 'C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe'
-toriyList = ["torrentleech.org", "iptorrents.com", "filelist.io", "cinemaZ.to", "avistaZ.to", "exoticaZ.to", "animetorrents.me", "myanonamouse.net", "audionews.org"]
+#Program: toriyChek v1.1
+#Author: Richard Ortiz
+#Updated: 04.13.24
 
 
-print(brave)
-print(toriyList)
-f = open("brave.exe")
+# Define variables and import module
+brave = 'C:\Program Files\BraveSoftware\Brave-Browser\Application\\brave.exe'
+siteList = ["torrentleech.org", "iptorrents.com", "filelist.io", "cinemaZ.to", "avistaZ.to", "exoticaZ.to", "animetorrents.me", "myanonamouse.net", "audionews.org"]
+index = 0
+counter = int(index) #convert string to integer
+import keyboard, time, os
 
-# Windows
+# Launch browser and wait
+os.startfile(brave)
+time.sleep(1)
+
+# For loop to launch websites sequentially (but opens one too many blank tabs still)
+for index in siteList:
+    time.sleep(1)
+    keyboard.write(siteList[counter])
+    keyboard.press_and_release('enter')
+    counter += 1
+    keyboard.press_and_release('ctrl+t')
+    time.sleep(1)
+    
+# Close browser once done
+if counter >= 9:
+    time.sleep(2)
+    keyboard.press_and_release('alt+f4')
+    
+
