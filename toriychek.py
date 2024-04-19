0660d7cbc94b842f61d2637fa678acf83cf9ec26
roartiz <63872939+roartiz@users.noreply.github.com>
2024-04-19 11:04:05 -0400

    Uploaded single-tab variant of script

--- toriychek.py
+++ toriychek.py
@@ -1,29 +1,27 @@
 #Program: toriyChek v1.1
 #Author: Richard Ortiz
-#Updated: 04.13.24
+#Updated: 04.17.24
 
 
 # Define variables and import modules
 brave = 'C:\Program Files\BraveSoftware\Brave-Browser\Application\\brave.exe'
 siteList = ["torrentleech.org", "iptorrents.com", "filelist.io", "cinemaZ.to", "avistaZ.to", "exoticaZ.to", "animetorrents.me", "myanonamouse.net", "audionews.org"]
-index = 0
-counter = int(index) #convert string to integer
+counter = 0
 import keyboard, time, os
 
 # Launch browser and wait
 os.startfile(brave)
 time.sleep(1)
 
-# For loop to launch websites sequentially (but opens one too many blank tabs still)
-for index in siteList:
+# While loop to launch websites sequentially
+while counter < 9:
     time.sleep(1)
     keyboard.write(siteList[counter])
     keyboard.press_and_release('enter')
+    time.sleep(2)
+    keyboard.press_and_release('f6')
     counter += 1
-    keyboard.press_and_release('ctrl+t')
-    time.sleep(1)
     
 # Close browser once done
-if counter >= 9:
-    time.sleep(2)
-    keyboard.press_and_release('alt+f4')
+if counter == 9:
+    keyboard.press_and_release('ctrl+w')
