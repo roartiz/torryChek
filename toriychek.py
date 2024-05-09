161c5125959916b715bc4f5a3502a451d9a0ff62
roartiz <63872939+roartiz@users.noreply.github.com>
2024-05-09 16:14:01 -0400

    Add files via upload
    Added pop-up dialog with last time program finished

--- toriychek.py
+++ toriychek.py
@@ -1,35 +1,39 @@
 """
 Program: toriyChek v1.34
 Author: Richard Ortiz
-Updated: 05.04.24
+Updated: 05.09.24
 """
 
 # Define variables and import modules
 import keyboard
 import time
 import os
+import ctypes
+import datetime
+
 
 brave = \
     'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'
 siteList = [
     'torrentleech.org',
     'iptorrents.com',
+    'filelist.io',
+    'cinemaZ.to',
     'avistaZ.to',
     'exoticaZ.to',
     'animetorrents.me',
     'myanonamouse.net',
-    'audionews.org',
-    'filelist.io'
-    'cinemaZ.to'
+    'audionews.org'
     ]
 counter = 0
+now = datetime.datetime.now()
 
 # Launch Brave after making sure it's closed
 os.system('taskkill /f /im brave.exe')
 os.startfile(brave)
 time.sleep(1)
 
-# For loop to launch websites sequentially, then close browser and kill process once done
+# For loop to launch sites sequentially, close browser and kill process, then display confirmation pop-up
 for _ in siteList:
     time.sleep(1)
     keyboard.write(siteList[counter])
@@ -39,8 +43,8 @@ for _ in siteList:
     counter += 1
     time.sleep(1)
     if counter == 9:
-        time.sleep(2)
         os.system('taskkill /f /im brave.exe')
+        ctypes.windll.user32.MessageBoxW(0, "Authentication has been completed at " + now.strftime("%H:%M") + "!", "Success", 0)
  
  
 """
@@ -48,13 +52,14 @@ backup siteList = [
     'torrentleech.org',
     'iptorrents.com',
     'filelist.io',
+    'cinemaZ.to',
     'avistaZ.to',
     'exoticaZ.to',
     'animetorrents.me',
     'myanonamouse.net',
     'audionews.org'
-    'cinemaZ.to'
     ]
+    
 fake siteList = [
     'torlami',
     'iptom',
